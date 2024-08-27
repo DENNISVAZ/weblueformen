aa66fbb0250694d53089e5cfec7e8358fb3bddc7
DENNISVAZ <35502117+DENNISVAZ@users.noreply.github.com>
2024-08-27 09:59:49 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -255,7 +255,7 @@ class FormSubmit {
   onSubmission(event) {
     event.preventDefault();
     event.target.disabled = true;
-    event.target.innerText = "EnviandoB...";
+    event.target.innerText = "Wait...";
 
 
   }
